900bfef84f9d04826be71da1ea616436b9c71791
John Fizz <aigner.jo@gmx.de>
2014-10-21 18:10:14 +0200

    A* implemented!

--- Thymio/src/context/Coordinate.java
+++ Thymio/src/context/Coordinate.java
@@ -3,6 +3,8 @@ package context;
 public class Coordinate {
 	private int x;
 	private int y;
+	private final double COSTFORTURN = 2;
+
 	// Test
 	public Coordinate(int x, int y) {
 		this.x = x;
@@ -15,5 +17,24 @@ public class Coordinate {
 	
 	public int getY() {
 		return y;
+	}
+	public double getDistanceTo(Node predecessor, Coordinate nextCoordinate) {
+		int x2 = nextCoordinate.getX();
+		int y2 = nextCoordinate.getY();
+		int additionalCost = 0;
+		if (predecessor != null) {
+			int x0 = predecessor.getCoordinate().getX();
+			int y0 = predecessor.getCoordinate().getY();
+			if (x0 == x && x != x2 || y0 == y && y != y2) {
+				additionalCost += COSTFORTURN;
+			}
+		}
+		double d = Math.sqrt(Math.pow((x2 - x), 2) + Math.pow((y2 - y), 2))
+				+ additionalCost;
+		return d;
+	}
+
+	public String toString() {
+		return x + "/" + y;
 	}
 }
